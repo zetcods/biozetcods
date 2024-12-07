06df05b8df3105ed7752a5825bc2eab829fd3259
ZetCods <ztrtylos@gmail.com>
2024-12-07 10:07:40 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Adding interactivity
+document.querySelectorAll(".social-btn").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      alert("Redirecting to social media...");
+    });
+  });
+  
+document.querySelectorAll(".link").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      alert("Redirecting to GameIn...");
+    });
+  });
+  
